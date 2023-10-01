c8a7ea3c7f04acbcb25cfd604bb1150def6a290f
Jongo Said (김정고) <31678025+saidijongo@users.noreply.github.com>
2023-10-02 05:37:23 +0900

    Create sinebeat16_bgreenFlash.ino

---
+++ sinebeat16_bgreenFlash.ino
@@ -0,0 +1,34 @@
+#include <FastLED.h>
+
+#define NUM_LEDS  6
+#define LED_PIN   8
+
+DEFINE_GRADIENT_PALETTE( browngreen_gp ) { 
+    0,    6,  255,    0,     //green
+   71,    0,  255,  153,     //bluegreen
+  122,  200,  200,  200,     //gray
+  181,  110,   61,    6,     //brown
+  255,    6,  255,    0      //green
+};
+
+CRGB leds[NUM_LEDS];
+CRGBPalette16 orangePink = browngreen_gp;
+
+void setup() {
+  
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(10);
+
+  Serial.begin(115200);
+
+  FastLED.clear(true);
+}
+
+void loop () {
+
+    uint16_t beatA = beatsin16(30, 0, 255);
+    uint16_t beatB = beatsin16(20, 0, 255);
+    fill_palette(leds, NUM_LEDS, (beatA + beatB) / 2, 10, orangePink, 255, LINEARBLEND);
+
+    FastLED.show();
+}
