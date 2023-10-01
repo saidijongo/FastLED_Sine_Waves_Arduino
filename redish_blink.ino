d4b734b68478be552351f795b3e872661ac65840
Jongo Said (김정고) <31678025+saidijongo@users.noreply.github.com>
2023-10-01 21:24:50 +0900

    Create redish_blink.ino

---
+++ redish_blink.ino
@@ -0,0 +1,28 @@
+#include<FastLED.h>
+
+#define LED_AMOUNT        30
+#define DATA_PIN          8
+#define GLOBAL_BRIGHTNESS 30
+
+CRGB leds[LED_AMOUNT];
+
+float animation = 0;
+
+void setup() {
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, LED_AMOUNT); 
+  FastLED.setBrightness(GLOBAL_BRIGHTNESS);
+  Serial.begin(115200);
+}
+
+void loop() {
+
+  for(int i=0; i<LED_AMOUNT; ++i){
+    float hue = sin(animation + (i+1) * 0.1);
+    hue = (hue + 1) / 2;
+    hue *= 255;
+    leds[i] = CHSV(hue, 255, 255);
+  }
+
+  FastLED.show();
+  animation += 0.05;
+}
