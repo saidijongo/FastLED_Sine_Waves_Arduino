d4d6b94cc07af8d80ec5300b03b1882048f53c5d
Jongo Said (김정고) <31678025+saidijongo@users.noreply.github.com>
2023-10-02 17:33:53 +0900

    Create midStop_slideOnBlack.ino

---
+++ midStop_slideOnBlack.ino
@@ -0,0 +1,39 @@
+#include <FastLED.h>
+
+#define NUM_LEDS  6
+#define LED_PIN   8
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(50);
+}
+
+void loop() {
+
+  // Waves for LED position
+  uint8_t posBeat  = beatsin8(30, 0, NUM_LEDS - 1, 0, 0);
+  uint8_t posBeat2 = beatsin8(60, 0, NUM_LEDS - 1, 0, 0);
+  uint8_t posBeat3 = beatsin16(30, 0, NUM_LEDS - 1, 0, 127);
+  uint8_t posBeat4 = beatsin16(60, 0, NUM_LEDS - 1, 0, 127);
+
+
+  // In the video I use beatsin8 for the positions. For longer strips,
+  // the resolution isn't high enough for position and can lead to some
+  // LEDs not lighting. If this is the case, use the 16 bit versions below
+  // uint16_t posBeat  = beatsin16(30, 0, NUM_LEDS - 1, 0, 0);
+  // uint16_t posBeat2 = beatsin16(60, 0, NUM_LEDS - 1, 0, 0);
+  // uint16_t posBeat3 = beatsin16(30, 0, NUM_LEDS - 1, 0, 32767);
+  // uint16_t posBeat4 = beatsin16(60, 0, NUM_LEDS - 1, 0, 32767);
+
+  // Wave for LED color
+  uint8_t colBeat  = beatsin8(45, 0, 255, 0, 0);
+
+  leds[(posBeat + posBeat2) / 2]  = CHSV(colBeat, 255, 255);
+  leds[(posBeat3 + posBeat4) / 2]  = CHSV(colBeat, 255, 255);
+
+  fadeToBlackBy(leds, NUM_LEDS, 10);
+
+  FastLED.show();
+}
