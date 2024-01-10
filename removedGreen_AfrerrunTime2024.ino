9bd5a81ce2f9d613ff4c7f2c6abfbdcc41285e15
Jongo Said (김정고) <31678025+saidijongo@users.noreply.github.com>
2024-01-10 16:18:25 +0900

    Create removedGreen_AfrerrunTime2024.ino

---
+++ removedGreen_AfrerrunTime2024.ino
@@ -0,0 +1,64 @@
+#include <FastLED.h>
+#include <elapsedMillis.h>
+
+#define LED_PIN 11
+#define NUM_LEDS 32
+
+CRGB leds[NUM_LEDS];
+
+elapsedMillis elapsedTime; // Declare elapsedTime as a static variable
+
+void setup() {
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+}
+
+void ledStrip(int speed, int runTime) {
+  unsigned long startTime = millis();
+  unsigned long currentTime;
+
+  // Set the background color to blue
+  fill_solid(leds, NUM_LEDS, CRGB::Blue);
+  FastLED.show();
+
+  while (elapsedTime < runTime) {
+    currentTime = millis(); // Update currentTime
+
+    // Move the group of 5 green LEDs from left to right
+    for (int i = 0; i <= NUM_LEDS - 5; ++i) {
+      fill_solid(leds + i, 5, CRGB::Green);
+      FastLED.show();
+      delay(speed);
+      fill_solid(leds + i, 5, CRGB::Blue);
+
+      // Check if runtime exceeded
+      if (currentTime - startTime >= runTime) {
+        FastLED.clear();
+        FastLED.show();
+        return;
+      }
+    }
+
+    // Move the group of 5 green LEDs from right to left
+    for (int i = NUM_LEDS - 5; i >= 0; --i) {
+      fill_solid(leds + i, 5, CRGB::Green);
+      FastLED.show();
+      delay(speed);
+      fill_solid(leds + i, 5, CRGB::Blue);
+
+      // Check if runtime exceeded
+      if (currentTime - startTime >= runTime) {
+        FastLED.clear();
+        FastLED.show();
+        return;
+      }
+    }
+  }
+
+  // Turn off LEDs after the specified duration
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
+  FastLED.show();
+}
+
+void loop() {
+  ledStrip(100, 2000);
+}
